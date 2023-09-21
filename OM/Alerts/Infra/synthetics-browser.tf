c750e9bc8806ef7c28cfa8a8d991611bdfb694fa
Manassevitz <manassevitz@gmail.com>
2023-09-21 18:08:13 +0200

    dinamyc synthetics module

--- OM/Alerts/Infra/synthetics-browser.tf
+++ OM/Alerts/Infra/synthetics-browser.tf
@@ -7,7 +7,8 @@ module "syntethics" {
   period           = "EVERY_10_MINUTES"
   locations_public = ["US_EAST_1"]
   folder           = "./scripts/"
-  type             = "SCRIPT_API"
+  type             = local.stack_instances[count.index].type // SCRIPT_BROWSER or SCRIPT_API
+  runtime_type_version = local.stack_instances[count.index].runtime_type_version // 1 for chrome 100+ - 2 form 72 legacy .. you can join to 2 to 1 but no 1 to 2 
   policy_id        = 4142478
 }
 
