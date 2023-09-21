62776693607c22a00b9d7f1c6cd88a7212c7931d
Manassevitz <manassevitz@gmail.com>
2023-09-21 17:43:02 +0200

    fix runtype

--- OM/Alerts/Infra/synthetics-browser.tf
+++ OM/Alerts/Infra/synthetics-browser.tf
@@ -1,12 +1,13 @@
 module "syntethics" {
-  count                = length(local.stack_instances)
-  source               = "../../../modules/syntethicsBrowser"
-  name                 = "Synthetics -  ${local.stack_instances[count.index].stack_name}"
-  file                 = local.stack_instances[count.index].stack_js
-  status               = "ENABLED"
-  period               = "EVERY_10_MINUTES"
-  locations_public     = ["US_EAST_1"]
-  folder               = "./scripts/"
-  policy_id             = 4142478
+  count            = length(local.stack_instances)
+  source           = "../../../modules/syntethicsBrowser"
+  name             = local.stack_instances[count.index].stack_name
+  file             = local.stack_instances[count.index].stack_js
+  status           = "ENABLED"
+  period           = "EVERY_10_MINUTES"
+  locations_public = ["US_EAST_1"]
+  folder           = "./scripts/"
+  type             = "SCRIPT_API"
+  policy_id        = 4142478
 }
 
